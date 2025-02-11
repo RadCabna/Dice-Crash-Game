70ae28b2ebe679501f66b627e45e3078a71d5247
Radcabna <radcabna@gmail.com>
2025-02-11 20:17:24 +0300

    Initial Commit

---
+++ Dice Crash Game/Dice_Crash_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Crash_GameApp.swift
+//  Dice Crash Game
+//
+//  Created by Алкександр Степанов on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Crash_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
