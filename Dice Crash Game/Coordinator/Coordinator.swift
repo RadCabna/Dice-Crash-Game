bc2ecaeab1fbb1ee01d4b7b67d1c8c1aaac669aa
Radcabna <radcabna@gmail.com>
2025-02-11 22:38:39 +0300

    first commit

---
+++ Dice Crash Game/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  Dice Crash Game
+//
+//  Created by Алкександр Степанов on 11.02.2025.
+//
+
+import Foundation
---
+++ Dice Crash Game/Views/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  Dice Crash Game
+//
+//  Created by Алкександр Степанов on 11.02.2025.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
